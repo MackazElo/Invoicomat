1ac1f5654eb966d6b74b798cac3fb4795450571d
MackazElo <mackaz2000@gmail.com>
2023-05-19 11:42:59 +0200

    Working Excel with auto SKU, not working auto name

--- index.js
+++ index.js
@@ -22,48 +22,7 @@ app.use("/", express.static("public"));
 app.use(fileUpload());
 
 app.post("/extract-text", (req, res) => {
-    async function get_sku(supplier, sku) {
-        function fetch_sku(supplier) {
-            return new Promise((resolve, reject) => {
-            var query = `SELECT sku FROM mobileparts WHERE supplier_code = "${supplier}"`;
-                    database.query(query, function(error, data){
-                            resolve(data)
-                        });
-            });
-        }
-        function update_sku(data) {
-            return new Promise((resolve, reject) => {
-                var query = `INSERT INTO mobileparts (id, supplier_code, sku) VALUES ("", "${supplier}", "${sku}")`;
-                        database.query(query, function(error, data){
-                            // console.log("SKU DB updated")
-                        
-                            resolve(sku)
-                        })
-                    })
-        }
-        
-        function select_sku(val) {
-            return new Promise((resolve, reject) => {
-                    var splited = stringify(val[0])
-                    splited = splited.split("=")
-                    // console.log("SKU found")
-                  
-                    resolve(splited[1])
-                        })
-        }
-    try {
-        var data = await fetch_sku(supplier);
-        if(data==""&&sku!=""){
-            data = await update_sku(supplier, sku); 
-        }
-        else{
-            data = await select_sku(data); 
-        }
-        return(data)
-    } catch (error) {
-        console.error('Error:', error);
-    }
-    }
+   
     async function get_name(sku) {
         sku = String(sku)
         sku = sku.replace(/%2F/g, "/");
@@ -190,7 +149,7 @@ app.post("/extract-text", (req, res) => {
                     }
                     // console.log(content)
                     async function create_table(){
-                            var table = `<form method="post" action="/excel" ><table id="invoice_table">`
+                            var table = `<form method="post" id="invoice_form" action="/excel" ><table id="invoice_table">`
                             
                             table +=`<tr><th>SKU/Name</th><th>Supplier Code</th><th>Description</th><th>Price</th><th>Quantity</th></tr>`
                             for(i=1;i<row_number; i++){
@@ -200,14 +159,20 @@ app.post("/extract-text", (req, res) => {
                                     content[i][2]=""
                                 }
                                 table +=`<tr><td><div class="autocomplete" ><input  id='${i}_0' name='r${i}_c0a' type="text"  value='${content[i][1]}'  placeholder="SKU"></div><br>`       
-                                table +=`<div class="autocomplete" ><input  id='${i}_0b' name='r${i}_c0' type="text"  value='${content[i][2]}'  placeholder="Part Name"></div></td>`
+                                table +=`<div class="autocomplete" ><input  id='${i}_0b' name='r${i}_c0b' type="text"  value='${content[i][2]}'  placeholder="Part Name"></div></td>`
                                 table +=`<td><input type='text' name='r${i}_c1' value='${content[i][0]}' hidden>${content[i][0]}</td>`
-                                table +=`<td><input type='text' name='r${i}_c1' value='${content[i][3]}' hidden>${content[i][3]}</td>`
-                                table +=`<td><input type='text' name='r${i}_c1' value='${content[i][4]}' hidden>${content[i][5]}</td>`
-                                table +=`<td><input type='text' name='r${i}_c1' value='${content[i][5]}' hidden>${content[i][4]}</td></tr>`
+                                table +=`<td><input type='text' name='r${i}_c2' value='${content[i][3]}' hidden>${content[i][3]}</td>`
+                                table +=`<td><input type='text' name='r${i}_c3' value='${content[i][4]}' hidden>${content[i][5]}</td>`
+                                table +=`<td><input type='text' name='r${i}_c4' value='${content[i][5]}' hidden>${content[i][4]}</td></tr>`
                                 
                             }
-                            table += `</table></form>`
+                            table += ` <input type="text" id="supplier" name="supplier" placeholder="supplier">
+                            <input type="text" id="invoice_number" name="invoice_number" placeholder="invoice_number">
+                            <input type="text" id="order_number" name="order_number" placeholder="order_number">
+                            <input type="text" name="last_serial" placeholder="last_serial">
+                            <input type='number' name='max_rows' id='max_rows' hidden>
+                                <input type="submit" id="submit_form" hidden>
+                                </table></form>`
                             // console.log(table)
                             // console.log(stringify(table))
                             return table
@@ -247,54 +212,18 @@ app.post("/extract-text", (req, res) => {
 });
 
 app.post("/excel", (req, res) => {
-    async function mainn(){
+    
+       
+    
+
+async function mainn(){
 
         
-        async function get_sku(supplier, sku) {
-            function fetch_sku(supplier) {
-                return new Promise((resolve, reject) => {
-                var query = `SELECT sku FROM mobileparts WHERE supplier_code = "${supplier}"`;
-                        database.query(query, function(error, data){
-                                resolve(data)
-                            });
-                });
-            }
-            function update_sku(data) {
-                return new Promise((resolve, reject) => {
-                    var query = `INSERT INTO mobileparts (id, supplier_code, sku) VALUES ("", "${supplier}", "${sku}")`;
-                            database.query(query, function(error, data){
-                                // console.log("SKU DB updated")
-                            
-                                resolve(sku)
-                            })
-                        })
-            }
-            
-            function select_sku(val) {
-                return new Promise((resolve, reject) => {
-                        var splited = stringify(val[0])
-                        splited = splited.split("=")
-                        // console.log("SKU found")
-                      
-                        resolve(splited[1])
-                            })
-            }
-        try {
-            var data = await fetch_sku(supplier);
-            if(data==""){
-                data = await update_sku(supplier, sku); 
-            }
-            else{
-                data = await select_sku(data); 
-            }
-            return(data)
-        } catch (error) {
-            console.error('Error:', error);
-        }
-        }
-        var last_serial =  String(req.body.last_serial);
+        
+    var last_serial =  String(req.body.last_serial);
 
-    var max_rows = String(req.body.max_rows);
+            
+    var max_rows =  String(req.body.max_rows);
 
     var excel = require('excel4node');
     // Create a new instance of a Workbook class
@@ -319,35 +248,38 @@ app.post("/excel", (req, res) => {
     worksheet.cell(1,8).string("Repair").style(style);
     worksheet.cell(1,9).string("Store").style(style);
 
+    async function prepare_content(){
+        for(i=1; i<max_rows; i++){
+        
+            var sku = String(eval(`req.body.r${i}_c0a`));
+            var name = String(eval(`req.body.r${i}_c0b`));
+            var code = String(eval(`req.body.r${i}_c1`));
+            var quantity = String(eval(`req.body.r${i}_c3`));
+            var price = String(eval(`req.body.r${i}_c4`));
+            console.log(quantity)
+            console.log(price)
+            price = price.replace('€ ', "");
+            price = price * 6 ;
+            // console.log(last_serial)
+            var serials = generate_serial(last_serial, quantity);
+            // console.log(last_serial)
+            await get_sku(code, sku)
     
-
-     for(i=1; i<=max_rows; i++){
-        var name = String(eval(`req.body.r${i}_c1`));
-        var sku = String(eval(`req.body.r${i}_c1a`));
-        var supplier = String(eval(`req.body.r${i}_c2`));
-        var price = String(eval(`req.body.r${i}_c4`));
-        var quantity = String(eval(`req.body.r${i}_c5`));
-        price = price.replace('€ ', "");
-        price = price * 6 ;
-        // console.log(last_serial)
-        var serials = generate_serial(last_serial, quantity);
-        // console.log(last_serial)
-
-        var corretct_sku = await (get_sku(supplier, sku))
-
-        worksheet.cell(i+1,1).string(name).style(style);
-        worksheet.cell(i+1,2).string(quantity).style(style);
-        worksheet.cell(i+1,3).string(serials).style(style);
-        worksheet.cell(i+1,4).string("").style(style);
-        worksheet.cell(i+1,5).string("").style(style);
-        worksheet.cell(i+1,6).number(price).style(style);
-        worksheet.cell(i+1,7).string("0").style(style);
-        worksheet.cell(i+1,8).string("0").style(style);
-        worksheet.cell(i+1,9).string("0").style(style);
-        worksheet.cell(i+1,10).string(corretct_sku).style(style);
+            worksheet.cell(i+1,1).string(name).style(style);
+            worksheet.cell(i+1,2).string(quantity).style(style);
+            worksheet.cell(i+1,3).string(serials).style(style);
+            worksheet.cell(i+1,4).string("").style(style);
+            worksheet.cell(i+1,5).string("").style(style);
+            worksheet.cell(i+1,6).number(price).style(style);
+            worksheet.cell(i+1,7).string("0").style(style);
+            worksheet.cell(i+1,8).string("0").style(style);
+            worksheet.cell(i+1,9).string("0").style(style);
+            worksheet.cell(i+1,9).string("0").style(style);
+        }
+    
     }
 
-    
+     await prepare_content()
     workbook.write('Excel2.xls')
    setTimeout(() => {  make_file(); }, 5000);
     function make_file(){
@@ -383,6 +315,48 @@ app.post("/excel", (req, res) => {
     mainn()
 });
 
+async function get_sku(supplier, sku) {
+    function fetch_sku(supplier) {
+        return new Promise((resolve, reject) => {
+        var query = `SELECT sku FROM mobileparts WHERE supplier_code = "${supplier}"`;
+                database.query(query, function(error, data){
+                        resolve(data)
+                    });
+        });
+    }
+    function update_sku(data) {
+        return new Promise((resolve, reject) => {
+            var query = `INSERT INTO mobileparts (id, supplier_code, sku) VALUES ("", "${supplier}", "${sku}")`;
+                    database.query(query, function(error, data){
+                        // console.log("SKU DB updated")
+                    
+                        resolve(sku)
+                    })
+                })
+    }
+    
+    function select_sku(val) {
+        return new Promise((resolve, reject) => {
+                var splited = stringify(val[0])
+                splited = splited.split("=")
+                // console.log("SKU found")
+              
+                resolve(splited[1])
+                    })
+    }
+try {
+    var data = await fetch_sku(supplier);
+    if(data==""&&sku!=""){
+        data = await update_sku(supplier, sku); 
+    }
+    else{
+        data = await select_sku(data); 
+    }
+    return(data)
+} catch (error) {
+    console.error('Error:', error);
+}
+}
 app.listen(5000);
 console.log("Started at port 5000");
 
